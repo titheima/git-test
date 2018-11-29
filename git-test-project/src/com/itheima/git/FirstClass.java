f41823bd3173ea97694604bcb435db322ecc8512
tt <944783128@qq.com>
2018-11-29 19:46:29 +0800

    把本地仓库添加到git仓库中

---
+++ git-test-project/src/com/itheima/git/FirstClass.java
@@ -0,0 +1,10 @@
+package com.itheima.git;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("使用idea添加到git本地仓库");
+        System.out.println("使用idea添加到git本地仓库");
+        System.out.println("使用idea添加到git本地仓库");
+        System.out.println("使用idea添加到git本地仓库");
+    }
+}
